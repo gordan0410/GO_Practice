c6ae4521df9db4185cf566ac73b82218797c1ddb
gordan_kuo <gordan_kuo@mail.chungyo.net>
2021-11-10 10:24:15 +0800

    final for second hw

--- hw_second_gocv_camera_capture/go.mod
+++ hw_second_gocv_camera_capture/go.mod
@@ -2,4 +2,4 @@ module tgk-git.xbb-slot.com/TGK_Training/gordan_kuo/hw_second_gocv_camera_captur
 
 go 1.17
 
-require gocv.io/x/gocv v0.28.0 // indirect
+require gocv.io/x/gocv v0.28.0
--- hw_second_gocv_camera_capture/gocv_camera_capture.go
+++ hw_second_gocv_camera_capture/gocv_camera_capture.go
@@ -1,51 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"gocv.io/x/gocv"
 )
 
-func close_time(sec int) (window_switch int) {
-	time.Sleep(time.Duration(sec) * time.Second)
-	window_switch = 0
-	return window_switch
-}
-
 func main() {
+
+	// 本機攝影機代號
 	device_id := 0
 
 	// 確認catch到攝影機
 	webcam, err := gocv.VideoCaptureDevice(device_id)
 	if err != nil {
-		fmt.Printf("Error opening video capture device: %v\n", device_id)
-		return
+		log.Fatalf("Error opening video capture device: %v\n", err)
 	}
+	// 等待鏡頭開啟（暖機）(約300ms抓500ms)
+	time.Sleep(500 * time.Millisecond)
 
-	// 新增Window and Mat
-	window := gocv.NewWindow("Hello")
+	// 新增Mat
 	img := gocv.NewMat()
 
-	//設置開關
-	window_switch := 1
-
-	if window_switch == 1 {
-		for i := 0; i < 100; i++ {
-			webcam.Read(&img)
-			if ok := webcam.Read(&img); !ok {
-				fmt.Printf("Erro reading the device : %v\n", device_id)
-			}
-			window.IMShow(img)
-			window.WaitKey(1)
-		}
-
-		close_time(1)
-		gocv.IMWrite("selfie.jpg", img)
-
-	} else if window_switch == 0 {
-		webcam.Close()
-		window.Close()
-		img.Close()
+	// 讀取畫面進Mat
+	webcam.Read(&img)
+	if ok := webcam.Read(&img); !ok {
+		log.Fatal("Erro happended in webcam.Read.\n")
 	}
+
+	// 照片儲存
+	gocv.IMWrite("selfie.jpg", img)
+
+	// 關機
+	// classifier.Close()
+	img.Close()
+	webcam.Close()
 }
