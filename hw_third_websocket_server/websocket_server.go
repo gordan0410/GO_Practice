5ba2b37b06ba4aad8734beb955cbf0afa666398f
gordan_kuo <gordan_kuo@mail.chungyo.net>
2021-11-10 10:36:13 +0800

    final for third hw

--- hw_third_websocket_server/websocket_server.go
+++ hw_third_websocket_server/websocket_server.go
@@ -1,30 +1,65 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 var err error
 
 func main() {
-	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/", page_render)
+	http.HandleFunc("/ws", ws_connect)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
-		log.Fatal("ListenAndServ: ", err)
+		log.Fatalf("ListenAndServ failed: ", err)
 	}
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+func page_render(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./render.html")
 	if err != nil {
-		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		log.Fatalf("template.ParseFiles failed: ", err)
 	}
-	time.Sleep(time.Second)
-	if err = c.WriteJSON("hello world"); err != nil {
-		log.Println(err)
+	t.Execute(w, nil)
+
+}
+
+func ws_connect(w http.ResponseWriter, r *http.Request) {
+	//判斷請求是否為websocket升級請求。
+	if websocket.IsWebSocketUpgrade(r) {
+		conn, err := upgrader.Upgrade(w, r, w.Header())
+		if err != nil {
+			log.Fatalf("websocket upgrader.Upgrade failed: ", err)
+		}
+		conn.WriteMessage(websocket.TextMessage, []byte("The web socket is connected."))
+		// 使用goroutine接收與回覆訊息
+		go func() {
+			for {
+				t, c, err := conn.ReadMessage()
+				if err != nil {
+					// panic(err)
+					// log.Fatalf("websocket conn.ReadMessage() failed: ", err)
+					return
+				}
+				received := fmt.Sprintf("Message: \"%s\" received.", string(c))
+				conn.WriteMessage(websocket.TextMessage, []byte(received))
+				if t == -1 {
+					return
+				}
+			}
+		}()
+	} else {
+		fmt.Println("not connected")
 	}
-	defer c.Close()
 }
